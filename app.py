e0d2f048dadf63eb1a17785f9916b1b04abddff8
LuisGomes18 <luiscgomes0@gmail.com>
2024-07-14 20:28:15 +0000

    Update da homepage

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
-from os import urandom
-from flask import Flask, render_template
+import requests
+from os import urandom, getenv
+from dotenv import load_dotenv
+from flask import Flask, render_template, abort
 from extras import carregar_configuracoes
 
 
 app = Flask(__name__)
 app.secret_key = urandom(16)
 confg = carregar_configuracoes()
+load_dotenv()
+ip_api = getenv('IP_API')
 
 
 @app.route('/')
 def index():
-    return render_template('index.html'), 200
+    response = requests.get(f"http://{ip_api}/api/posts")
+    if response.status_code == 200:
+        posts = response.json()
+        return render_template('index.html', posts=posts), 200
+    else:
+        abort(500)
+
+@app.route('/login')
+def login():
+    return render_template('login.html'), 200
 
 
 if __name__ == '__main__':
