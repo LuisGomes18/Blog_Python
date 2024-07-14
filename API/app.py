9d604675f8484e205edbd838c18e6826ebed9245
LuisGomes18 <luiscgomes0@gmail.com>
2024-07-14 21:35:31 +0000

    Melhoramento nos status com apresentacao de erros e correcao de portugues

--- API/app.py
+++ API/app.py
@@ -1,22 +1,29 @@
 from os import urandom
 import time
+from datetime import datetime
 from flask import Flask, jsonify, request, abort
 from extras import carregar_configuracoes, conectar_banco_de_dados
 
 
 app = Flask(__name__)
 app.secret_key = urandom(16)
-confg = carregar_configuracoes()
+config = carregar_configuracoes()
 start_time = time.time()
+internal_errors = []
 
 def get_uptime():
     current_time = time.time()
     uptime_seconds = int(current_time - start_time)
     return uptime_seconds
 
+
 @app.route('/api/status', methods=['GET'])
 def index():
-    return jsonify({'message': 'API Rodando normalmente'}), 200
+    uptime_seconds = get_uptime()
+    status_message = {'message': 'API Rodando normalmente', 'uptime_seconds': uptime_seconds}
+    if internal_errors:
+        status_message['internal_errors'] = internal_errors
+    return jsonify(status_message), 200
 
 @app.route('/api/uptime', methods=['GET'])
 def get_uptime_api():
@@ -25,23 +32,23 @@ def get_uptime_api():
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
-    coneccao = conectar_banco_de_dados()
-    cursor = coneccao.cursor(dictionary=True)
+    connection = conectar_banco_de_dados()
+    cursor = connection.cursor(dictionary=True)
     cursor.execute("SELECT * FROM posts")
     posts = cursor.fetchall()
     cursor.close()
-    coneccao.close()
+    connection.close()
     return jsonify(posts), 200
 
 @app.route('/api/post', methods=['GET'])
 def get_post():
     id = request.args.get('id')
-    conexao = conectar_banco_de_dados()
-    cursor = conexao.cursor(dictionary=True)
+    connection = conectar_banco_de_dados()
+    cursor = connection.cursor(dictionary=True)
     cursor.execute("SELECT * FROM posts WHERE id = %s", (id,))
     post = cursor.fetchone()
     cursor.close()
-    conexao.close()
+    connection.close()
     if post:
         return jsonify(post), 200
     else:
@@ -52,13 +59,16 @@ def get_post():
 def not_found(error):
     return jsonify({'message': 'Requisição inválida'}), 404
 
-@app.errorhandler(500)
-def internal_error(error):
+@app.errorhandler(Exception)
+def handle_exception(error):
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    error_message = f'Erro em {now}: {str(error)}'
+    internal_errors.append(error_message)
     return jsonify({'message': 'Erro interno'}), 500
 
 
 if __name__ == '__main__':
-    debug = confg['informacao_api']['debug']
-    host = confg['informacao_api']['host']
-    port = confg['informacao_api']['port']
+    debug = config['informacao_api']['debug']
+    host = config['informacao_api']['host']
+    port = config['informacao_api']['port']
     app.run(debug=debug, host=host, port=port)
