a2de5e206858730e3c1da4c7fc20b2a041e766e1
LuisGomes18 <luiscgomes0@gmail.com>
2024-07-14 20:43:19 +0000

    Adição da API

---
+++ API/app.py
@@ -0,0 +1,38 @@
+from os import urandom
+from flask import Flask, jsonify
+from extras import carregar_configuracoes, conectar_banco_de_dados
+
+app = Flask(__name__)
+app.secret_key = urandom(16)
+confg = carregar_configuracoes()
+
+
+@app.route('/api')
+def index():
+    return jsonify({'message': 'API Rodando normalmente'}), 200
+
+@app.route('/api/posts', methods=['GET'])
+def get_posts():
+    coneccao = conectar_banco_de_dados()
+    cursor = coneccao.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM posts")
+    posts = cursor.fetchall()
+    cursor.close()
+    coneccao.close()
+    return jsonify(posts), 200
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({'message': 'Requisição inválida'}), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return jsonify({'message': 'Erro interno'}), 500
+
+
+if __name__ == '__main__':
+    debug = confg['informacao_api']['debug']
+    host = confg['informacao_api']['host']
+    port = confg['informacao_api']['port']
+    app.run(debug=debug, host=host, port=port)
---
+++ API/extras.py
@@ -0,0 +1,62 @@
+from os import getenv
+from dotenv import load_dotenv
+from yaml import safe_load, YAMLError
+import mysql.connector
+
+
+load_dotenv()
+db_host = getenv('DB_HOST')
+db_user = getenv('DB_USER')
+db_pass = getenv('DB_PASS')
+db_name = getenv('DB_NAME')
+db_config = {
+    'user': db_user,
+    'password': db_pass,
+    'host': db_host,
+    'database': 'BLOG'
+}
+
+
+def carregar_configuracoes():
+    """
+    Carrega as configurações do arquivo config/config.yaml.
+
+    Retorna as configurações em forma de dicionário.
+    Em caso de erro, lança uma exceção.
+
+    :return: Dicionário com as configurações
+    :raises YAMLError: Se houver um erro ao ler o arquivo
+    :raises FileNotFoundError: Se o arquivo não for encontrado
+    :raises Exception: Se houver outro erro
+    """
+
+    # Abrimos o arquivo config/config.yaml na modalidade de leitura
+    try:
+        with open('../config/config.yaml', 'r', encoding='utf-8') as f:
+
+            # Utilizamos a função safe_load para ler o arquivo YAML
+            # e converter as configurações em um dicionário.
+            # Se houver um erro ao ler o arquivo, lança uma exceção
+            # do tipo YAMLError.
+            return safe_load(f)
+
+    # Se o arquivo não for encontrado, lança uma exceção do tipo FileNotFoundError.
+    except FileNotFoundError:
+
+        # Se houver outro erro, lança uma exceção genérica com uma mensagem
+        # de erro detalhada.
+        raise Exception(f'Erro ao ler o arquivo config.yml: Arquivo não encontrado')
+
+    # Se houver um erro ao ler o arquivo YAML, lança uma exceção do tipo YAMLError.
+    except YAMLError:
+        raise YAMLError('Erro ao ler o arquivo config.yml')
+
+    # Se houver outro erro, lança uma exceção genérica com uma mensagem
+    # de erro detalhada.
+    except Exception as e:
+        raise Exception(f'Erro ao ler o arquivo config.yml: {e}')
+
+
+def conectar_banco_de_dados():
+    conececao = mysql.connector.connect(**db_config)
+    return conececao
