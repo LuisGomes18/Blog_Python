8a4c3c68d6f3c8aac98d407b122fcad02a6dadf2
LuisGomes18 <luiscgomes0@gmail.com>
2024-07-16 17:38:31 +0000

    Update do caminho

--- app.py
+++ app.py
@@ -63,11 +63,11 @@ def logout():
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html'), 404
+    return render_template('error/404.html'), 404
 
 @app.errorhandler(500)
 def internal_error(error):
-    return render_template('500.html'), 500
+    return render_template('error/500.html'), 500
 
 
 if __name__ == '__main__':
